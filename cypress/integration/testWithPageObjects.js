b82c0457bcb7f8ce9181b8247ea274690d7629d9
Areid0093 <54606271+Areid0093@users.noreply.github.com>
2022-03-23 02:45:10 -0500

    tests

--- cypress/integration/testWithPageObjects.js
+++ cypress/integration/testWithPageObjects.js
@@ -6,7 +6,7 @@ const { onSmartTablePage } = require("../support/page_objects/smartTablePage");
 describe('Test with Page Objects', () => {
     
     beforeEach('open application', () => {
-        cy.visit('/')
+        cy.openHomePage()
     })
 
     it('verify navigations across the pages', () => {
